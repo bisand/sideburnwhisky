b72d32f607fa2baed5e2c2772c42717473cfa27b
André Biseth <andre@biseth.net>
2021-10-11 12:55:29 +0200

    Included auth0

--- sideburn-web/src/app/app.module.ts
+++ sideburn-web/src/app/app.module.ts
@@ -6,16 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
+import { AuthModule } from '@auth0/auth0-angular';
+import { AuthButtonComponentComponent } from './auth-button-component/auth-button-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthButtonComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    AuthModule.forRoot({
+      domain: 'bisand.auth0.com',
+      clientId: 'PJeWwDMvJmzo25dz8M1EgnK8txLlwyGF'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ sideburn-web/src/app/auth-button-component/auth-button-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-button-component',
+  templateUrl: './auth-button-component.component.html',
+  styleUrls: ['./auth-button-component.component.css']
+})
+export class AuthButtonComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
