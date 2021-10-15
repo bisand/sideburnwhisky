a5afcf9209b0846634511c7447746a51c03a1bf4
André Biseth <andre@biseth.net>
2021-10-15 08:02:49 +0200

    Modified nav bar

--- sideburn-web/src/app/app.component.ts
+++ sideburn-web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sideburn-web';
+  title = 'Sideburn Whiskylaug';
+  public isMenuCollapsed = true;
+
+  constructor(public auth: AuthService){
+    
+  }
+
+  loginWithRedirect(): void {
+    this.auth.loginWithRedirect();
+  }
+
+  logoutWithRedirect(): void {
+    // const url = this.auth.buildLogoutUrl({ returnTo: `${window.location.origin + '/portfolio/'}` }).subscribe(url => {
+    // });
+    this.auth.logout({ returnTo: `${window.location.origin + '/portfolio/'}` });
+  }
+
 }
