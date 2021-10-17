ba76282b123891ad0e820150b1f583b5a941eaf3
André Biseth <andre@biseth.ney>
2021-10-17 22:15:44 +0200

    Couchdb work in progress

--- sideburn-api/server.js
+++ sideburn-api/server.js
@@ -1,15 +1,40 @@
 'use strict';
-
+require('dotenv').config();
 const express = require('express');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+const NodeCouchDb = require('node-couchdb');
+
+// node-couchdb instance talking to external service
+const couch = new NodeCouchDb({
+  host: 'localhost',
+  protocol: 'http',
+  port: 5984,
+  auth: {
+    user: process.env.COUCHDB_USER,
+    pass: process.env.COUCHDB_PASSWORD
+  }
+});
+
+
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get('/', async (req, res) => {
+  try {
+    couch.listDatabases()
+      .then(
+        dbs => {
+          res.send(dbs);
+        },
+        err => {
+          res.send(err);
+        });
+  } catch (error) {
+    res.send(JSON.stringify(error));
+  }
 });
 
 app.listen(PORT, HOST);
