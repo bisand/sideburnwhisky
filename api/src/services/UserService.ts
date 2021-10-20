79d36e4bec0125197d76fbb1cb1ff565c5008ff5
André Biseth <andre@biseth.net>
2021-10-20 09:28:36 +0200

    Added view insert.

--- api/src/services/UserService.ts
+++ api/src/services/UserService.ts
@@ -2,19 +2,9 @@ import { User } from '../models/User';
 import { IDataService } from './IDataService';
 
 export class UserService {
-    private createViews(){
-        // function (doc) { 
-        //     if (doc.type === "user" && doc.active) { 
-        //       emit(doc.username, doc)
-        //     }
-        //   }
-        // Use this view to retrieve active Users
-        //this._dataService.db.createIndex()
-    }
-
     async getUsers(): Promise<User[]> {
         try {
-            const response = await this._dataService.db.view("sideburn", "users-all");
+            const response = await this._dataService.db.view("sideburn-users", "all");
             const result: User[] = [];
             response.rows.forEach(doc => {
                 result.push(Object.assign({} as User, doc));
@@ -28,7 +18,7 @@ export class UserService {
 
     async getActiveUsers(): Promise<User[]> {
         try {
-            const response = await this._dataService.db.view("sideburn", "users-active");
+            const response = await this._dataService.db.view("sideburn-users", "active");
             const result: User[] = [];
             response.rows.forEach(doc => {
                 result.push(Object.assign({} as User, doc));
@@ -53,5 +43,46 @@ export class UserService {
     }
     constructor(dataService: IDataService) {
         this._dataService = dataService;
+        this.createViews();
+    }
+
+    private async createViews() {
+        // function (doc) { 
+        //     if (doc.type === "user" && doc.active) { 
+        //       emit(doc.username, doc)
+        //     }
+        //   }
+        // Use this view to retrieve acti ve Users
+        const allUsers = `function (doc) {
+            if (doc.type === "user") { 
+                emit(doc.username, doc)
+            }
+        }`;
+        const activeUsers = `function (doc) {
+            if (doc.type === "user" && doc.active) { 
+                emit(doc.username, doc)
+            }
+        }`;
+
+        // Design Document
+        const ddoc = {
+            _id: '_design/sideburn-users',
+            views: {
+                'active': {
+                    map: activeUsers,
+                    // reduce: '_sum'
+                },
+                'all': {
+                    map: allUsers,
+                    // reduce: '_sum'
+                },
+            },
+            options: {
+                // partitioned: true
+            }
+        }
+
+        // create design document
+        await this._dataService.db.insert(ddoc);
     }
 }
