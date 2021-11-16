e5eb478c11b5fb1a2bbd38bd11ad7529879b31df
André Biseth <andre@biseth.net>
2021-11-16 13:53:28 +0100

    Added alerts

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import { LocalAuthService } from './services/local-auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Sideburn Whiskylaug';
 
-  constructor(public auth: AuthService){
-    
+  @ViewChild('alertSuccess', { static: false }) alertSuccess: NgbAlert | undefined;
+  @ViewChild('alertWarning', { static: false }) alertWarning: NgbAlert | undefined;
+  @ViewChild('alertError', { static: false }) alertError: NgbAlert | undefined;
+
+  constructor(public auth: LocalAuthService) {
+    auth.successMessage = ' ';
+    auth.warningMessage = ' ';
+    auth.errorMessage = ' ';
+  }
+  ngAfterViewInit(): void {
+    this.auth.alertSuccess = this.alertSuccess;
+    this.auth.alertWarning = this.alertWarning;
+    this.auth.alertError = this.alertError;
+    this.auth.successMessage = '';
+    this.auth.warningMessage = '';
+    this.auth.errorMessage = '';
   }
 }
--- web/src/app/services/local-auth.service.ts
+++ web/src/app/services/local-auth.service.ts
@@ -1,17 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { IdToken, LogoutOptions, User } from '@auth0/auth0-spa-js';
-import { Observable, of, Subscription } from 'rxjs';
+import { Observable, of, Subject, Subscription } from 'rxjs';
 import { UserService } from './user.service';
 import jwt_decode from "jwt-decode";
 import { Article } from '../models/Article';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import { debounceTime } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LocalAuthService {
+export class LocalAuthService implements OnInit {
   private _accessToken: string = '';
   private _parsedToken: any = {};
+  private _success = new Subject<string>();
+  private _warning = new Subject<string>();
+  private _error = new Subject<string>();
+
+  alertSuccess: NgbAlert | undefined;
+  alertWarning: NgbAlert | undefined;
+  alertError: NgbAlert | undefined;
 
   claims: IdToken | undefined | null;
   user: User | null | undefined;
@@ -19,6 +28,10 @@ export class LocalAuthService {
   profile: User | null | undefined;
 
   private _checker = (arr: string[], target: string[]) => target?.every(v => arr?.includes(v));
+
+  public successMessage?: string;
+  public warningMessage?: string;
+  public errorMessage?: string;
 
   public hasPermission(permissions: string[]): Boolean {
     return this._checker(this._parsedToken.permissions, permissions);
@@ -75,7 +88,29 @@ export class LocalAuthService {
     this._authService.loginWithRedirect();
   }
 
+  ngOnInit(): void {
+  }
+
   constructor(private _authService: AuthService, private _userService: UserService) {
+    this._success.subscribe(message => this.successMessage = message);
+    this._success.pipe(debounceTime(5000)).subscribe(() => {
+      if (this.alertSuccess) {
+        this.alertSuccess.close();
+      }
+    });
+    this._warning.subscribe(message => this.warningMessage = message);
+    this._success.pipe(debounceTime(5000)).subscribe(() => {
+      if (this.alertWarning) {
+        this.alertWarning.close();
+      }
+    });
+    this._error.subscribe(message => this.errorMessage = message);
+    this._success.pipe(debounceTime(5000)).subscribe(() => {
+      if (this.alertError) {
+        this.alertError.close();
+      }
+    });
+
     this._authService.error$.subscribe(error => {
     });
     this._authService.isAuthenticated$.subscribe(isAuthenticated => {
@@ -86,18 +121,22 @@ export class LocalAuthService {
     this._authService.user$.subscribe(user => {
       this.profile = user;
       this.user = user;
+      this._success.next(`user ${user?.email} - successfully authenticated.`);
     }, error => {
+      this._warning.next(`Login required: ${error}.`);
       console.log('Login required.')
     });
     this._authService.idTokenClaims$.subscribe((claims) => {
       this.claims = claims;
     }, error => {
+      this._warning.next(`Login required: ${error}.`);
       console.log('Login required.')
     });
     this._authService.getAccessTokenSilently({}).subscribe(token => {
       this._accessToken = token;
       this._parsedToken = jwt_decode(this._accessToken);
     }, error => {
+      this._warning.next(`Login required: ${error}.`);
       console.log('Login required.')
     });
   }
